160f7226abbbac7db3807e2bab799dfa89bd0bc0
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-06-26 16:35:44 +0200

    feat: application logbook
    changes:
    - logbook model tests added
    - logbook view model tests added

---
+++ tests/BB84.SAU.Application.Tests/ViewModels/LogbookViewModelTests.cs
@@ -0,0 +1,23 @@
+﻿using BB84.SAU.Application.Interfaces.Application.Services;
+using BB84.SAU.Application.ViewModels;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+namespace BB84.SAU.Application.Tests.ViewModels;
+
+[TestClass]
+public sealed class LogbookViewModelTests
+{
+	[TestMethod]
+	public void LogbookViewModelTest()
+	{
+		Mock<INotificationService> notificationServiceMock = new();
+
+		LogbookViewModel viewModel = new(notificationServiceMock.Object);
+
+		Assert.IsNotNull(viewModel);
+		Assert.AreEqual(default, viewModel.LogbookEntries.Count);
+	}
+}
