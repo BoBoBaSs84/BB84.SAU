77708983117b129e5e58efdc16969a4084a6c7df
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-06-27 16:47:47 +0200

    feat: more abstractions
    changes:
    - added the `FileProvider` class
    - added the `IFileProvider' interface
    - added the `SteamWorksProvider` class
    - added the `ISteamWorksProvider' interface

---
+++ src/BB84.SAU.Infrastructure/Interfaces/Provider/IFileProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace BB84.SAU.Infrastructure.Interfaces.Provider;
+
+/// <summary>
+/// The file provider interface.
+/// Serves for the abstraction of <see cref="File"/> methods.
+/// </summary>
+internal interface IFileProvider
+{
+	/// <inheritdoc cref="File.WriteAllText(string, string?)"/>
+	void WriteAllText(string path, string? contents);
+
+	/// <inheritdoc cref="File.Delete(string)"/>
+	void Delete(string path);
+}
