4a7444e2502ce18c08c663b1e76d528402530e48
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-06-29 10:11:30 +0200

    fix: notification system
    changes:
    - the `INotificationService` interface modified
    - the `NotificationService` class modifed
    - small fixes applied
    
    closes #20

--- src/BB84.SAU.Application/Interfaces/Application/Services/INotificationService.cs
+++ src/BB84.SAU.Application/Interfaces/Application/Services/INotificationService.cs
@@ -8,22 +8,42 @@ namespace BB84.SAU.Application.Interfaces.Application.Services;
 public interface INotificationService
 {
 	/// <summary>
-	/// The event handler for message receiving.
+	/// The event handler for asynchronous message receiving.
 	/// </summary>
-	event AsyncNotificationEventHandler? MessageReceived;
+	event AsyncNotificationEventHandler? AsyncNotificationReceived;
 
 	/// <summary>
-	/// Sends a <paramref name="message"/> to all the subscribers.
+	/// The event handler for synchronous message receiving.
+	/// </summary>
+	event NotificationEventHandler? NotificationReceived;
+
+	/// <summary>
+	/// Sends a synchronous <paramref name="message"/> to all the subscribers.
 	/// </summary>
 	/// <param name="message">The message to send.</param>
 	void Send(string message);
+
+	/// <summary>
+	/// Sends a asynchronous <paramref name="message"/> to all the subscribers.
+	/// </summary>
+	/// <param name="message">The message to send.</param>
+	/// <returns><see cref="Task"/></returns>
+	Task SendAsync(string message);
 }
 
 /// <summary>
-/// Represents the method that will handle an notification event when the event provides message data.
+/// Represents the method that will handle an asynchronous notification event when the event provides message data.
 /// </summary>
 /// <param name="sender">The source of the event.</param>
 /// <param name="message">An <see cref="string"/> that contains the event message.</param>
 /// <returns><see cref="Task"/></returns>
 [SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "I want it that way.")]
 public delegate Task AsyncNotificationEventHandler(object sender, string message);
+
+/// <summary>
+/// Represents the method that will handle an synchronous notification event when the event provides message data.
+/// </summary>
+/// <param name="sender">The source of the event.</param>
+/// <param name="message">An <see cref="string"/> that contains the event message.</param>
+[SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "I want it that way.")]
+public delegate void NotificationEventHandler(object sender, string message);
